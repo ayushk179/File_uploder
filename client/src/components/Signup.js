b1b63bf69f5c9dc99014415f83f301669055fa70
ayush <ayushkanojia179@gmail.com>
2023-10-08 20:04:14 +0530

    final

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -29,7 +29,7 @@ export default function SignUp() {
       })
         .then((res) => res.json())
         .then((data) => {
-          console.log(data, "userRegister");
+          
           if (data.status == "ok") {
             alert("Registration Successful");
              window.localStorage.setItem("token", data.data);
